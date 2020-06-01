d881e937b5e68204be45faa5679bbd3f4f20ad66
Matthew Lluis <mwlluis@hotmail.com>
2020-06-01 09:59:44 -0400

    the app now has all the business logic to test a leap year by divisibility by 4, 100, and 400

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 const leapYear = function(year) {
   
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
